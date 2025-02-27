15fe80b99ccfe5304b3766fc0b66dcfd1c4e7941
Fernando Morrison <fmorri@gmail.com>
2025-02-27 20:15:33 +0100

    now using AuthProvider with props

--- context/AuthProvider.js
+++ context/AuthProvider.js
@@ -2,18 +2,5 @@ import { useState, useContext } from "react";
 import AuthContext from "./AuthContext";
 
 export default function AuthProvider({ children, value }) {
-  const [username, setUsername] = useState(null);
-  const [token, setToken] = useState(null);
-
-  function login(username, token) {
-    setUsername(username);
-    setToken(token);
-  }
-
-  function logout() {
-    setUsername(null);
-    setToken(null);
-  }
-
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
